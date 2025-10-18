bee9185ea9e7eac2b75edf716dfcf62cd9391d0d
us6ikn <121174383+us6ikn@users.noreply.github.com>
2025-10-18 12:45:32 +0200

    Update L.WAZ.js

--- leaflet-waz/L.WAZ.js
+++ leaflet-waz/L.WAZ.js
@@ -1,40 +1,26 @@
-// L.WAZ.js
-L.WAZ = L.Layer.extend({
-  options: {
-    geojsonUrl: null,  // URL to your GeoJSON
-    style: { color: 'blue', weight: 2, fillOpacity: 0.2 }
-  },
-
-  initialize: function(options) {
-    L.setOptions(this, options);
-    this._layer = L.geoJSON(null, {
-      style: this.options.style,
-      onEachFeature: this._onEachFeature
-    });
-  },
-
-  onAdd: function(map) {
-    this._map = map;
-    this._layer.addTo(map);
-    if (this.options.geojsonUrl) {
-      fetch(this.options.geojsonUrl)
-        .then(res => res.json())
-        .then(data => this._layer.addData(data));
-    }
-  },
-
-  onRemove: function() {
-    this._map.removeLayer(this._layer);
-  },
-
-  _onEachFeature: function(feature, layer) {
-    if (feature.properties && feature.properties.name) {
-      layer.bindPopup(feature.properties.name);
-    }
-  }
-});
-
-// Factory function
 L.waz = function(options) {
-  return new L.WAZ(options);
+  const geojsonLayer = L.geoJSON(null, {
+    style: function(feature) {
+      if (feature.geometry.type === "Polygon") {
+        return { color: 'blue', weight: 2, fillOpacity: 0.1 }; // Boundary styling
+      }
+      return {}; // Points don't need a style
+    },
+    pointToLayer: function(feature, latlng) {
+      return L.marker(latlng).bindPopup(feature.properties.name); // Show labels
+    },
+    onEachFeature: function(feature, layer) {
+      if (feature.properties && feature.properties.name) {
+        layer.bindPopup(feature.properties.name);
+      }
+    }
+  });
+
+  if (options.geojsonUrl) {
+    fetch(options.geojsonUrl)
+      .then(res => res.json())
+      .then(data => geojsonLayer.addData(data));
+  }
+
+  return geojsonLayer;
 };
