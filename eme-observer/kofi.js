9cd347a735db5961efe67f93c007fc124a3f6576
us6ikn <121174383+us6ikn@users.noreply.github.com>
2026-01-05 22:43:17 +0100

    v0

--- eme-observer/kofi.js
+++ eme-observer/kofi.js
@@ -1,6 +1,6 @@
 kofiWidgetOverlay.draw('sa5ikn', {
   'type': 'floating-chat',
-  'floating-chat.donateButton.text': '',
+  'floating-chat.donateButton.text': 'Support',
   'floating-chat.donateButton.background-color': '#00b9fe',
   'floating-chat.donateButton.text-color': '#fff'
 });
